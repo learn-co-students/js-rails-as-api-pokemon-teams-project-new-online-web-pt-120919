6e73818769615b54c18744f039d2d1e0582f5e52
Robert Shields <robert.shieldsiv@gmail.com>
2020-07-10 21:52:57 -0400

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -23,13 +23,37 @@ const loadTrainers = () => {
         const ul = document.createElement("ul")
 
         div.setAttribute("class", "card")
-        div.setAttribute("data-id", "trainerHash["id"]")
+        div.setAttribute("data-id", trainerHash["id"])
         p.innerHTML = trainerHash.name
         button.setAttribute("data-trainer-id", trainerHash.id)
         button.innerHTML = "Add Pokemon"
+        button.addEventListener("click", createPokemon)
 
         div.appendChild(p)
         div.appendChild(button)
         div.appendChild(ul)
-        
+
+        main.appendChild(div)
+        trainerHash.pokemons.forEach(pokemon => renderPokemon(pokemon))
+    }
+
+    const renderPokemon = (pokemon) => {
+        const ul = document.querySelector(`div[data-id="${pokemon.trainer_id}"]`)
+        const li = document.createElement("li")
+        const button = document.createElement("button")
+
+        li.innerHTML = `${pokemon.nickname}(${pokemon.species})`
+        button.setAttribute("class" , "release")
+        button.setAttribute("data-pokemon-id" , "pokemon.id")
+        button.innerHTML = "Release"
+        button.addEventListener("click", destroyPokemon)
+
+        li.appendChild(button)
+        ul.appendChild(li)
+
+    }
+
+
+    const createPokemon = () => {
+
     }
