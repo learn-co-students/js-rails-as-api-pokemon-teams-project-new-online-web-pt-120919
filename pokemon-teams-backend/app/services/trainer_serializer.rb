e286fecacf9a9eaa47622ce4e50e0c7597ede821
aschonely83 <aschonely@gmail.com>
2020-07-15 12:59:57 -0400

    done

---
+++ pokemon-teams-backend/app/services/trainer_serializer.rb
@@ -0,0 +1,18 @@
+class TrainerSerializer
+
+  def initialize(trainer_obj)
+    @trainer = trainer_obj
+  end
+  
+  def to_serialized_json
+    options = {
+        include: {
+            pokemons: {
+                only: [:id, :nickname, :species, :trainer_id]
+            }
+        },
+        except: [:created_at, :update_at]
+    }
+    @trainer.to_json(options)
+  end  
+end    
