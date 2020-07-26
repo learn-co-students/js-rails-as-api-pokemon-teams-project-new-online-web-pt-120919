4b25d5f3783575fbac3e3c6ec28795dfba9d842f
cesmiley <catherinesmiley@gmail.com>
2020-07-25 20:06:32 -0400

    Done.

---
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -0,0 +1,17 @@
+class TrainersController < ApplicationController
+
+    def index
+        trainers = Trainer.all
+        render json: trainers.to_json(:include => {
+            :pokemons => {:except => [:created_at, :updated_at]}
+        }, except: [:created_at, :updated_at])
+    end 
+
+    def show 
+        trainer = Trainer.find(params[:id])
+        render json: trainer.to_json(:include => {
+            :pokemons => {:except => [:created_at, :updated_at]}
+        }, except: [:created_at, :updated_at])
+    end 
+
+end
